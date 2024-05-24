23ad44d8980a4676293c803d232365dbaafd5686
Bill Sideris <bill88t@feline.gr>
2024-05-24 14:12:21 +0300

    Add mpy support for generation

--- scripts/generate_package.py
+++ scripts/generate_package.py
@@ -1,17 +1,17 @@
-print("[1/3] Loading jz")
+print("[3/4] Loading jz")
 from sys import path
 
 path.insert(1, "submodules/jz")
 del path
 from jz import compress
 
-print("[2/3] Building package")
+print("[4/4] Building package")
 from os import listdir, chdir
 
 chdir("files")
 execstr = ""
 for filee in listdir():
     execstr += f", '{filee}'"
-execstr = "compress(" + execstr[2:] + ", '../package.jpk')"  # fancy bullshittery
+execstr = "compress(" + execstr[2:] + ", '../package.jpk')"
 exec(execstr)
 print("Done.")
